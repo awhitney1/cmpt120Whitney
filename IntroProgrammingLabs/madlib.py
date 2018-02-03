c59fac3900391caea8bc06880c4b5bdb6d23e732
Andrew Whitney <andrew.whitney1@marist.edu>
2018-02-02 21:06:33 -0500

    solution to Mad Libs exercise.

---
+++ IntroProgrammingLabs/madlib.py
@@ -0,0 +1,10 @@
+# Andrew Whitney
+# Intro to Programming
+
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adverb = input("Enter an adverb: ")
+print("Your MadLib:")
+print("The", adjective, noun, verb, adverb, "under the quick red fox.")
+    
