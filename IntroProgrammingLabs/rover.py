22a8bff70177b2df06c44a7e4c81f7a695214255
Andrew Whitney <andrew.whitney1@marist.edu>
2018-02-02 19:13:37 -0500

    Solution to Curiosity rover exercise.

---
+++ IntroProgrammingLabs/rover.py
@@ -0,0 +1,11 @@
+# Andrew Whitney
+# Introduction to Programming
+# A program calculating the time it takes to send pictures from a mars rover to earth
+
+def main():
+    distance = (34000000)
+    speed = (186000)
+    time = (distance/speed)
+    print(time)
+
+main()
